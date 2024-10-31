6a736e5a308e090918f7e2973c609f6dfa909557
kwillydot1 <william.kaidussis@truesec.se>
2024-10-31 15:36:44 +0100

    Update main.tf
    version two troubleshooting shit

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -358,7 +358,7 @@ resource "aws_launch_template" "ecs_launch_template" {
   network_interfaces {
     security_groups = [aws_security_group.ecs_sg.id]
   }
-  user_data = data.template_file.user_data.rendered
+  user_data = base64encode(data.template_file.user_data.rendered)
 }
 resource "aws_autoscaling_group" "ecs_asg" {
   name                 = "ECS-lab-asg"
